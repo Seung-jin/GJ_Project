77dcedaa1b3b615371fbc20efa4a53871e66e8bf
LSJ <fewfree12@naver.com>
2018-01-03 20:13:36 +0900

    가챠 신 이름 변경

--- GJProject/Assets/Script/GameManager/GachaCharacterSceneScript.cs
+++ GJProject/Assets/Script/GameManager/GachaCharacterSceneScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GachaCharacterScript : MonoBehaviour
+public class GachaCharacterSceneScript : MonoBehaviour
 {
     public Text messageText;
     public Text displayText;
