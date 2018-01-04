57cdc80f298eb2b1edbf299ee88c723ea69fe78b
LSJ <fewfree12@naver.com>
2018-01-04 23:18:10 +0900

    CharacterObject 스크립트를 Character스크립트와 합치고 제거

--- GJProject/Assets/Script/Character/CharacterManagerScript.cs
+++ GJProject/Assets/Script/Character/CharacterManagerScript.cs
@@ -7,23 +7,13 @@ public class CharacterManagerScript : MonoBehaviour
     private static CharacterManagerScript _instance = null;
     public static CharacterManagerScript Instance() { return _instance; }
 
+    //characterObjectScript로 변경할 것
     public List<CharacterScript> characters = new List<CharacterScript>();
-
-    public void CreateCharacter()
-    {
-        characters.Add(new Character_Dealer1());
-        characters.Add(new Character_Tanker1());
-        characters.Add(new Character_Healer1());
-    }
 
     void Awake()
     {
         if (_instance == null)
             _instance = this;
-    }
-
-    void Start()
-    {
-        CreateCharacter();
+        DontDestroyOnLoad(this);
     }
 }
--- GJProject/Assets/Script/Character/CharacterScript.cs
+++ GJProject/Assets/Script/Character/CharacterScript.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterScript {
+public class CharacterScript : MonoBehaviour {
     public int max_hp;
     public int cur_hp;
     public int normal_attack;
     public List<CharacterSkillScript> skillList = new List<CharacterSkillScript>();
     public string cha_name;
     public Grade grade;
+
+    public Animation characterAni;
+    public AnimationClip attackAniClip;
+
+    void Start()
+    {
+        IdleAnimation();
+    }
+
+    public void IdleAnimation()
+    {
+        characterAni.Play("idle");
+    }
+
+    public void Attack()
+    {
+        characterAni.Stop();
+        characterAni.Play(attackAniClip.name);
+        Invoke("IdleAnimation", attackAniClip.length);
+    }
 }
 
 public enum Grade
